c54ac245519ca1f56bd48865d7eb5d597cfa00ca
Wei Lun <weilun_95@hotmail.com>
2018-03-11 20:31:36 +0800

    tested for exception

--- src/test/com/example/workshop/TestWorkshop.java
+++ src/test/com/example/workshop/TestWorkshop.java
@@ -1,6 +1,8 @@
 package com.example.workshop;
 
 import com.company.workshop.domain.Controller;
+import com.company.workshop.domain.MaxParticipantsException;
+import com.company.workshop.domain.Workshop;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,5 +13,13 @@ public class TestWorkshop {
         Controller controller = new Controller();
         controller.registerWorkshop("c++", 10);
         assertEquals(controller.searchWorkshop("c++").getTitle(), "c++");
+    }
+
+    @Test(expected = MaxParticipantsException.class)
+    public void testMaxParticipant() throws MaxParticipantsException {
+        Workshop workshop = new Workshop("c++", 1);
+        workshop.addParticipant("weilun");
+        workshop.addParticipant("wanching");
+
     }
 }
